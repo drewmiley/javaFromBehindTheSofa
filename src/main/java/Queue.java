87f2affcf9578abb3fa6759c5ac890212d134dbc
Drew Miley <dmiley@scottlogic.co.uk>
2017-03-11 23:51:26 +0000

    Adds empty methods in Queue

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -1,2 +1,14 @@
 public class Queue {
+
+    public void insert() {
+
+    }
+
+    public void remove() {
+
+    }
+
+    public void examine() {
+
+    }
 }
--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -1,5 +1,6 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +13,24 @@ public class QueueTest {
     @After
     public void tearDown() throws Exception {
 
+    }
+
+    @Test
+    public void testHasInsertMethod() {
+        Queue queue = new Queue();
+        queue.insert();
+    }
+
+    @Test
+    public void testHasRemoveMethod() {
+        Queue queue = new Queue();
+        queue.remove();
+    }
+
+    @Test
+    public void testHasExamineMethod() {
+        Queue queue = new Queue();
+        queue.examine();
     }
 
 }
