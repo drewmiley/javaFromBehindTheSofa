d52229d06a77011dbecb6ae274b4734b2e9d0aa2
Drew Miley <dmiley@scottlogic.co.uk>
2017-03-12 13:04:37 +0000

    Queue implemented

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -1,16 +1,46 @@
 public class Queue {
 
-    public void insert(int element) {
+    private int[] queueArray = new int[1];
 
+    public void insert(int element) {
+        if (queueArray[queueArray.length - 1] != 0) {
+            int[] queueArrayMem = queueArray;
+            queueArray = new int[queueArrayMem.length * 2];
+            for (int i = 0; i < queueArrayMem.length; i++) {
+                queueArray[i] = queueArrayMem[i];
+            }
+            queueArray[queueArrayMem.length] = element;
+            return;
+        } else if (queueArray.length == 1 && queueArray[0] == 0) {
+            queueArray[0] = element;
+            return;
+        } else {
+            for (int i = queueArray.length - 1; i >= 0; i--) {
+                if (queueArray[i] != 0) {
+                    queueArray[i + 1] = element;
+                    return;
+                }
+            }
+        }
     }
 
     public int remove() {
-
+        for (int i = queueArray.length - 1; i >= 0; i--) {
+            if (queueArray[i] != 0) {
+                int value = queueArray[i];
+                queueArray[i] = 0;
+                return value;
+            }
+        }
         return 0;
     }
 
     public int examine() {
-
+        for (int i = queueArray.length - 1; i >= 0; i--) {
+            if (queueArray[i] != 0) {
+                return queueArray[i];
+            }
+        }
         return 0;
     }
 }
