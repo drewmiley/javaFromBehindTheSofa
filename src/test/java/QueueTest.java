3c2ff70ea281abf532320ecc2f4988f285a4408f
Drew Miley <dmiley@scottlogic.co.uk>
2017-03-12 00:03:59 +0000

    Adds empty tests for Queue functionality

--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -45,4 +45,86 @@ public class QueueTest {
         int value = queue.examine();
     }
 
+    @Test
+    public void testInsertMethodAddsIntToQueue() throws Exception {
+        Queue queue = new Queue();
+        queue.insert(1);
+        int value = queue.examine();
+        assertEquals(1, value, 0);
+
+        queue.insert(2);
+        value = queue.examine();
+        assertEquals(2, value, 0);
+
+        queue.insert(3);
+        value = queue.examine();
+        assertEquals(3, value, 0);
+    }
+
+    @Test
+    public void testRemoveMethodRemovesIntFromQueue() throws Exception {
+        Queue queue = new Queue();
+        queue.insert(1);
+        queue.insert(2);
+        int value = queue.examine();
+        assertEquals(2, value, 0);
+        queue.remove();
+        value = queue.examine();
+        assertEquals(1, value, 0);
+
+        queue.insert(3);
+        queue.insert(4);
+        value = queue.examine();
+        assertEquals(4, value, 0);
+        queue.remove();
+        value = queue.examine();
+        assertEquals(3, value, 0);
+    }
+
+    @Test
+    public void testRemoveMethodReturnsQueuedInt() throws Exception {
+        Queue queue = new Queue();
+        queue.insert(1);
+        queue.insert(2);
+        int value = queue.remove();
+        assertEquals(2, value, 0);
+
+        queue.insert(3);
+        queue.insert(4);
+        value = queue.remove();
+        assertEquals(4, value, 0);
+    }
+
+    @Test
+    public void testExamineMethodReturnsQueuedInt() throws Exception {
+        Queue queue = new Queue();
+        queue.insert(1);
+        queue.insert(2);
+        int value = queue.examine();
+        assertEquals(2, value, 0);
+
+        queue.insert(3);
+        queue.insert(4);
+        value = queue.examine();
+        assertEquals(4, value, 0);
+    }
+
+    @Test
+    public void testExamineMethodDoesNotRemoveIntFromQueue() throws Exception {
+        Queue queue = new Queue();
+        queue.insert(1);
+        queue.insert(2);
+        int value = queue.examine();
+        assertEquals(2, value, 0);
+        value = queue.examine();
+        assertEquals(2, value, 0);
+
+        queue.insert(3);
+        queue.insert(4);
+        value = queue.examine();
+        assertEquals(4, value, 0);
+        value = queue.examine();
+        assertEquals(4, value, 0);
+    }
+
 }
