a5d38d5262cc5f35bae8a1f0137ca94fc115c7a3
Drew Miley <dmiley@scottlogic.co.uk>
2017-03-12 13:49:48 +0000

    Adds empty methods in ArraySort

--- src/main/java/ArraySort.java
+++ src/main/java/ArraySort.java
@@ -1,2 +1,40 @@
 public class ArraySort {
+
+    private int[] array;
+
+    public ArraySort(int[] array) {
+        this.array = array;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+    public int[] ascVOne() {
+        return sortOne(array, true);
+    }
+
+    public int[] descVOne() {
+        return sortOne(array, false);
+    }
+
+    private int[] sortOne(int[] array, boolean ascending) {
+        return array;
+    }
+
+    public int[] ascVTwo() {
+        return sortTwo(array, true);
+    }
+
+    public int[] descVTwo() {
+        return sortTwo(array, false);
+    }
+
+    private int[] sortTwo(int[] array, boolean ascending) {
+        return array;
+    }
 }
--- src/test/java/ArraySortTest.java
+++ src/test/java/ArraySortTest.java
@@ -1,5 +1,6 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +13,68 @@ public class ArraySortTest {
     @After
     public void tearDown() throws Exception {
 
+    }
+
+    @Test
+    public void testHasIntArrayConstructor() {
+        int[] array = new int[3];
+        ArraySort arraySort = new ArraySort(array);
+    }
+
+    @Test
+    public void testHasAscVOneMethod() {
+        int[] array = new int[3];
+        ArraySort arraySort = new ArraySort(array);
+        arraySort.ascVOne();
+    }
+
+    @Test
+    public void testHasDescVOneMethod() {
+        int[] array = new int[3];
+        ArraySort arraySort = new ArraySort(array);
+        arraySort.descVOne();
+    }
+
+    @Test
+    public void testHasAscVTwoMethod() {
+        int[] array = new int[3];
+        ArraySort arraySort = new ArraySort(array);
+        arraySort.ascVTwo();
+    }
+
+    @Test
+    public void testHasDescVTwoMethod() {
+        int[] array = new int[3];
+        ArraySort arraySort = new ArraySort(array);
+        arraySort.descVTwo();
+    }
+
+    @Test
+    public void testAscVOneMethodReturnsIntArray() {
+        int[] array = new int[3];
+        ArraySort arraySort = new ArraySort(array);
+        int[] ascArray = arraySort.ascVOne();
+    }
+
+    @Test
+    public void testDescVOneMethodReturnsIntArray() {
+        int[] array = new int[3];
+        ArraySort arraySort = new ArraySort(array);
+        int[] descArray = arraySort.descVOne();
+    }
+
+    @Test
+    public void testAscVTwoMethodReturnsIntArray() {
+        int[] array = new int[3];
+        ArraySort arraySort = new ArraySort(array);
+        int[] ascArray = arraySort.ascVTwo();
+    }
+
+    @Test
+    public void testDescVTwoMethodReturnsIntArray() {
+        int[] array = new int[3];
+        ArraySort arraySort = new ArraySort(array);
+        int[] descArrary= arraySort.descVTwo();
     }
 
 }
