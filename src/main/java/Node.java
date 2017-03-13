334c7d7d2b39500c80c93fd89a527c4bbe71a958
Drew Miley <drewmiley@Administrators-MacBook-Pro.local>
2017-03-13 15:04:40 +0000

    Adds Node object

---
+++ src/main/java/Node.java
@@ -0,0 +1,30 @@
+public class Node {
+
+    private int value;
+    private boolean black;
+    private int[] children = new int[2];
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isBlack() {
+        return black;
+    }
+
+    public void setBlack(boolean black) {
+        this.black = black;
+    }
+
+    public int[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(int[] children) {
+        this.children = children;
+    }
+}
